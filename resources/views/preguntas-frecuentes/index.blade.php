7fbbab7fba165a5198ba8ec3765de9e8638cbeee
Jean Luca Lapadula <jeanlucalapadula75@gmail.com>
2026-03-06 12:07:48 -0500

    Corregir layout de vistas preguntas frecuentes para rama develop

--- resources/views/preguntas-frecuentes/index.blade.php
+++ resources/views/preguntas-frecuentes/index.blade.php
@@ -1,9 +1,6 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Preguntas Frecuentes
-        </h2>
-    </x-slot>
+@extends('layouts.app')
+
+@section('content')
 
     <div class="p-6">
         <div class="max-w-7xl mx-auto space-y-6">
@@ -128,4 +125,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
