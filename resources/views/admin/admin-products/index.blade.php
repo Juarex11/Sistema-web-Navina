2caf16f00ae1ddea5b0cbfda8021c393821d9d63
JoSG7 <josegallodiaz3@gmail.com>
2026-03-14 12:37:00 -0500

    fix tailwind import

--- resources/views/admin/admin-products/index.blade.php
+++ resources/views/admin/admin-products/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="flex-1 overflow-auto px-6 py-7">
         <div class="max-w-6xl mx-auto">
-            <h1 class="!text-5xl !font-semibold !font-vibes pb-2">
+            <h1 class="text-5xl font-semibold font-vibes pb-2">
                 Gestión de productos
             </h1>
             <p class="text-gray-400 font-mulish">
@@ -15,7 +15,7 @@
 
                 <form action="{{ route('admin.products') }}" method="GET" class="mb-4 flex gap-2 py-2">
                     <input type="text" name="search" value="{{ request('search') }}" placeholder="Buscar por nombre o categoría..." class="px-4 py-2 border rounded-lg shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-pink-400">
-                        <button type="submit" class="px-4 py-2 bg-pink-400 hover:bg-pink-500 hover:shadow-lg hover:-translate-y-1 text-white !rounded-lg transition-all">
+                        <button type="submit" class="px-4 py-2 bg-pink-400 hover:bg-pink-500 hover:shadow-lg hover:-translate-y-1 text-white rounded-lg transition-all">
                             Buscar
                         </button>
                 </form>
@@ -87,7 +87,7 @@
                     <div class="mt-4">
                         {{ $products->links() }}
                     </div>
-                    <!-- {{-- Edit modal --}}
+                    <-- {{-- Edit modal --}}
                         <div class="modal fade" id="editModal" tabindex="-1">
                             <div class="modal-dialog modal-dialog-centered modal-lg">
                                 <div class="modal-content">
@@ -194,7 +194,7 @@
             </div>
 
             <div class="flex justify-end gap-3 mb-6 py-4 mx-14">
-                <button class="bg-pink-400 shadow-md hover:bg-pink-500 hover:shadow-lg hover:-translate-y-1 transition all text-white !rounded-full w-10 h-10 flex items-center justify-center" data-bs-toggle="modal" data-bs-target="#createModal">
+                <button class="bg-pink-400 shadow-md hover:bg-pink-500 hover:shadow-lg hover:-translate-y-1 transition all text-white rounded-full w-10 h-10 flex items-center justify-center" data-bs-toggle="modal" data-bs-target="#createModal">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
@@ -324,34 +324,34 @@
                                                 </div>
                                                 {{-- INFO --}}
                                                 <div>
-                                                    <h1 id="showName" class="mb-1 !text-3xl !font-extrabold !text-gray-600 !font-mulish"></h1>
+                                                    <h1 id="showName" class="mb-1 text-3xl font-extrabold text-gray-600 font-mulish"></h1>
                                                     <div class="mb-1">
-                                                        <h5 class="!font-bold !text-pink-500 !font-mulish">DESCRIPCIÓN:</h5>
+                                                        <h5 class="font-bold text-pink-500 font-mulish">DESCRIPCIÓN:</h5>
                                                         <p id="showDescription"></p>
                                                     </div>
                                                     <div class="mb-1">
-                                                        <h5 class="!font-bold !text-pink-500 !font-mulish">BENEFICIOS:</h5>
+                                                        <h5 class="font-bold text-pink-500 font-mulish">BENEFICIOS:</h5>
                                                         <p  style="white-space: pre-line;" id="showBenefits"></p>
                                                     </div>
                                                     <div class="mb-1">
-                                                        <h5 class="!font-bold !text-pink-500 !font-mulish">ESTADO:</h5>
+                                                        <h5 class="font-bold text-pink-500 font-mulish">ESTADO:</h5>
                                                         <div class="flex">
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                                                         </svg>
-                                                        <p class="!font-bold" id="showStatus"></p>
+                                                        <p class="font-bold" id="showStatus"></p>
                                                         </div>
                                                     </div>
                                                     <div class="mb-1">
-                                                        <h5 class="!font-bold !text-pink-500 !font-mulish">CATEGORÍA:</h5>
-                                                        <p class="!font-bold" id="showCategory"></p>
+                                                        <h5 class="font-bold text-pink-500 font-mulish">CATEGORÍA:</h5>
+                                                        <p class="font-bold" id="showCategory"></p>
                                                     </div>
                                                     <div class="mb-1 grid grid-cols-2">
                                                         <div class="flex">
                                                             <p class="font-extrabold text-pink-500 font-mulish">S/.</p>
                                                             <p id="showFinalPrice" class="font-extrabold text-pink-500 font-mulish px-2"></p>
-                                                            <s class="!text-sm font-extrabold text-gray-300 font-mulish line-through ml-2">S/.</s>
-                                                            <s id="showPrice" class="!text-sm font-extrabold text-gray-300 font-mulish line-through ml-2"></s>
+                                                            <s class="text-sm font-extrabold text-gray-300 font-mulish line-through ml-2">S/.</s>
+                                                            <s id="showPrice" class="text-sm font-extrabold text-gray-300 font-mulish line-through ml-2"></s>
                                                         </div>  
                                                     </div>
                                                 </div>
