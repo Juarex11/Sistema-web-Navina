b8063509554cf93b5fa3ac24292da61916f2c17f
Aamir Valentin <aamirvalentin718@gmail.com>
2026-04-07 20:46:31 -0500

    .

--- resources/js/components/banner.js
+++ resources/js/components/banner.js
@@ -1,16 +1,12 @@
 import Swiper from 'swiper';
 import 'swiper/css';
 
-const el = document.querySelector('.swiper');
-
-if (el) {
-    new Swiper(el, {
-        loop: true,
-        pagination: { el: '.swiper-pagination' },
-        navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
-    });
-}
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: { el: '.swiper-pagination' },
+    navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+    autoplay: {
+    delay: 3000, // tiempo entre slides en ms (3 segundos)
+    disableOnInteraction: false, // para que no se detenga al usar los botones
+    },
+});
